e8a6486cab1e6526d6294cd1c15bda12e2cd71f7
Student <ekbstudent@yandex-team.ru>
2022-02-08 00:22:06 +0500

    готовое ДЗ

---
+++ src/main/java/com/example/spring_boot/config/ProjectConfig.java
@@ -0,0 +1,23 @@
+package com.example.spring_boot.config;
+
+import com.example.spring_boot.DevProfile;
+import com.example.spring_boot.ProductionProfile;
+import com.example.spring_boot.SystemProfile;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+    @Bean
+    @ConditionalOnProperty(prefix = "profile", name = "dev", havingValue = "true")
+    public SystemProfile devProfile() {
+        return new DevProfile();
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "profile", name = "dev", havingValue = "false")
+    public SystemProfile prodProfile() {
+        return new ProductionProfile();
+    }
+}
